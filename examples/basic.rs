44bb3a4de50e5d8b1a65b5b5ba4c30d34e5288c8
Erik Vroon <erik.vroon@channable.com>
2026-01-15 18:45:48 +0100

    Bevy 0.18

--- examples/basic.rs
+++ examples/basic.rs
@@ -17,7 +17,7 @@ fn main() {
 
 fn setup_camera(mut commands: Commands<'_, '_>) {
     commands.spawn((
-        Camera::default(),
+        Camera3d::default(),
         Transform::from_xyz(10.0, 10.0, 10.0).looking_at(Vec3::ZERO, Vec3::Y),
         WhereWasI::camera(),
     ));
